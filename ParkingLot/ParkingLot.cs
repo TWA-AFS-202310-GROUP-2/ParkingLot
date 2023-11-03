d7cff16ba414feff8433a833549d02d176780199
Zliu53 <ZLiu53@slb.com>
2023-11-03 15:59:28 +0800

    Feat&Test: Achieve Story-1 AC-2

--- ParkingLot/ParkingLot.cs
+++ ParkingLot/ParkingLot.cs
@@ -1,20 +1,26 @@
-﻿namespace Parking
+﻿using System.Collections.Generic;
+
+namespace Parking
 {
     using System;
     public class ParkingLot
     {
         private string car;
+        private Dictionary<string,string> ticket2Car = new Dictionary<string, string>();
+
         public ParkingLot() { }
 
         public string fetch(string ticket)
         {
+            string car = ticket2Car[ticket];
             return car;
         }
 
         public string Park(string car)
         {
-            this.car = car;
-            return "q";
+            string ticket = "T-"+car;
+            ticket2Car.Add(ticket,car);
+            return ticket;
         }
     }
 }
--- ParkingLotTest/ParkingLotTest.cs
+++ ParkingLotTest/ParkingLotTest.cs
@@ -14,7 +14,19 @@ namespace ParkingLotTest
             string car = parkingLot.fetch(ticket);
 
             Assert.Equal("car",car);
+        }
 
+        [Fact]
+        public void Should_get_the_correct_car_when_fetch_car_by_ticket()
+        {
+            ParkingLot parkingLot = new ParkingLot();
+
+            string ticket1 = parkingLot.Park("car1");
+            string ticket2 = parkingLot.Park("car2");
+            string car1 = parkingLot.fetch(ticket1);
+            string car2 = parkingLot.fetch(ticket2);
+            Assert.Equal("car1", car1);
+            Assert.Equal("car2",car2);
         }
     }
 }
