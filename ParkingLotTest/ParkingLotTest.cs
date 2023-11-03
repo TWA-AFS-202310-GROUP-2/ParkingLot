1005e4563801260372a509d6c0af7ae574387f66
Xueqian Li <XLi112@slb.com>
2023-11-03 15:58:55 +0800

    feat: Every customer can park his/her car into the parking lot. And can fetch right car using correspond ticket

--- ParkingLotTest/ParkingLotTest.cs
+++ ParkingLotTest/ParkingLotTest.cs
@@ -15,17 +15,17 @@ namespace ParkingLotTest
             Assert.Equal(carNumber, car);
         }
 
-        //[Theory]
-        //[InlineData("car1", "car2")]
-        //public void Should_get_correct_car_when_fetch_with_corresponding_ticket(string carNumber1, string carNumber2)
-        //{
-        //    ParkingLot parkingLot = new ParkingLot();
-        //    string ticket = parkingLot.Park(carNumber1);
-        //    string ticket2 = parkingLot.Park(carNumber2);
-        //    string car = parkingLot.FetchCar(ticket);
-        //    string car2 = parkingLot.FetchCar(ticket2);
-        //    Assert.Equal(carNumber1, car);
-        //    Assert.Equal(carNumber2, car2);
-        //}
+        [Theory]
+        [InlineData("car1", "car2")]
+        public void Should_get_correct_car_when_fetch_with_corresponding_ticket(string carNumber1, string carNumber2)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park(carNumber1);
+            string ticket2 = parkingLot.Park(carNumber2);
+            string car = parkingLot.FetchCar(ticket);
+            string car2 = parkingLot.FetchCar(ticket2);
+            Assert.Equal(carNumber1, car);
+            Assert.Equal(carNumber2, car2);
+        }
     }
 }
