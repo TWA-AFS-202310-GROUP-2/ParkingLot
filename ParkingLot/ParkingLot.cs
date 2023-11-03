62c0b7ec67118f922761d94ce53250c5cada3230
Xueqian Li <XLi112@slb.com>
2023-11-03 15:58:24 +0800

    feat: park car and fetch correct car using ticket

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,34 @@
+﻿namespace ParkingLot
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ParkingLot
+    {
+        private readonly int capacity = 10;
+        private readonly HashSet<string> usedTickets = new HashSet<string>();
+        private readonly Dictionary<string, string> ticketCarMap = new Dictionary<string, string>();
+        private int parkingCount = 0;
+
+        public string Park(string carNumber)
+        {
+            if (parkingCount >= capacity)
+            {
+                Console.WriteLine("No available position. Cannot park more cars.");
+                return null;
+            }
+
+            var ticket = Guid.NewGuid().ToString();
+            ticketCarMap[ticket] = carNumber;
+            parkingCount++;
+
+            return ticket;
+        }
+
+        public string FetchCar(string ticket)
+        {
+            usedTickets.Add(ticket);
+            return ticketCarMap[ticket];
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,31 @@
+namespace ParkingLotTest
+{
+    using ParkingLot;
+    using Xunit;
+
+    public class ParkingLotTest
+    {
+        [Theory]
+        [InlineData("car")]
+        public void Should_get_the_same_car_when_FetchCar_by_ticket(string carNumber)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            string ticket = parkingLot.Park(carNumber);
+            string car = parkingLot.FetchCar(ticket);
+            Assert.Equal(carNumber, car);
+        }
+
+        //[Theory]
+        //[InlineData("car1", "car2")]
+        //public void Should_get_correct_car_when_fetch_with_corresponding_ticket(string carNumber1, string carNumber2)
+        //{
+        //    ParkingLot parkingLot = new ParkingLot();
+        //    string ticket = parkingLot.Park(carNumber1);
+        //    string ticket2 = parkingLot.Park(carNumber2);
+        //    string car = parkingLot.FetchCar(ticket);
+        //    string car2 = parkingLot.FetchCar(ticket2);
+        //    Assert.Equal(carNumber1, car);
+        //    Assert.Equal(carNumber2, car2);
+        //}
+    }
+}
