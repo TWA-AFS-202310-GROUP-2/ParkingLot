3b5ac0176590ced1bae5bdf782a515f6332c76bd
Yiyang Zhang <YZhang184@slb.com>
2023-11-04 16:31:04 +0800

    feat: implement story1 AC1-5 and add tests

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,40 @@
+﻿namespace ParkingLot
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ParkingLot
+    {
+        private readonly int capacity;
+        private readonly Dictionary<Ticket, Car> parkedCars;
+
+        public ParkingLot(int capacity)
+        {
+            this.capacity = capacity;
+            parkedCars = new Dictionary<Ticket, Car>();
+        }
+
+        public Ticket Park(Car car)
+        {
+            if (parkedCars.Count >= capacity || car == null)
+            {
+                return null;
+            }
+
+            var ticket = new Ticket();
+            parkedCars.Add(ticket, car);
+            return ticket;
+        }
+
+        public Car Fetch(Ticket ticket)
+        {
+            if (ticket == null || !parkedCars.TryGetValue(ticket, out var car))
+            {
+                return null;
+            }
+
+            parkedCars.Remove(ticket);
+            return car;
+        }
+    }
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,60 @@
+namespace ParkingLotTest
+{
+    using ParkingLot;
+    using Xunit;
+
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_return_parking_ticket_When_park_Given_parking_lot_and_car()
+        {
+            var parkingLot = new ParkingLot(10);
+            var car = new Car();
+            var ticket = parkingLot.Park(car);
+            Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void Should_return_parked_car_When_fetch_Given_parking_lot_with_parked_car_and_parking_ticket()
+        {
+            var parkingLot = new ParkingLot(10);
+            var car = new Car();
+            var ticket = parkingLot.Park(car);
+            var fetchedCar = parkingLot.Fetch(ticket);
+            Assert.Equal(car, fetchedCar);
+        }
+
+        [Fact]
+        public void Should_return_null_When_fetch_Given_a_wrong_ticket()
+        {
+            var parkingLot = new ParkingLot(10);
+            var car = new Car();
+            parkingLot.Park(car);
+            var wrongTicket = new Ticket();
+            var fetchedCar = parkingLot.Fetch(wrongTicket);
+            Assert.Null(fetchedCar);
+        }
+
+        [Fact]
+        public void Should_return_null_When_fetch_Given_used_a_ticket()
+        {
+            var parkingLot = new ParkingLot(10);
+            var car = new Car();
+            var ticket = parkingLot.Park(car);
+            parkingLot.Fetch(ticket); // Simulate fetching the car
+            var fetchedCar = parkingLot.Fetch(ticket); // Attempt to fetch with the same ticket
+            Assert.Null(fetchedCar);
+        }
+
+        [Fact]
+        public void Should_return_null_When_park_Given_a_full_parkingLot()
+        {
+            var parkingLot = new ParkingLot(1); // Parking lot with capacity of 1
+            var car1 = new Car();
+            var car2 = new Car();
+            parkingLot.Park(car1); // Park first car
+            var ticket = parkingLot.Park(car2); // Attempt to park another car
+            Assert.Null(ticket);
+        }
+    }
+}
